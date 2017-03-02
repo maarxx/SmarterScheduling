fe84aa8fa07d48673391bef11aed735f5d42aec9
maarxx <chris.lewark@gmail.com>
2017-03-02 00:01:27 -0500

    Okay we're back on track.

--- SmarterScheduling/SmarterScheduling/MapComponent_Brain.cs
+++ SmarterScheduling/SmarterScheduling/MapComponent_Brain.cs
@@ -7,6 +7,8 @@ namespace SmarterScheduling
 {
     class MapComponent_Brain : MapComponent
     {
+        int slowDown = 0;
+
         public MapComponent_Brain(Map map) : base(map)
         {
         }
@@ -14,12 +16,28 @@ namespace SmarterScheduling
         public override void MapComponentTick()
         {
             base.MapComponentTick();
+
+            //Log.Message("Hello, FAST!");
             // WOW that is FAST lets slow it down a little.
-            Log.Message("Hello, world!");
+
+            slowDown++;
+            if (slowDown < 100)
+            {
+                return;
+            }
+            else
+            {
+                slowDown = 0;
+            }
+
+            Log.Message("Hello, slow! MapID:" + map.uniqueID + ", ColonistCount:" + map.mapPawns.FreeColonistsSpawnedCount);
+            // OKAY that should be better.
+
+            /*
             foreach (Pawn p in map.mapPawns.FreeColonistsSpawned)
             {
-
             }
+            */
         }
     }
 }
