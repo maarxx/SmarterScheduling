674ca02711d53c2aa17114a2d87b79344c8dc5d9
maarxx <chris.lewark@gmail.com>
2017-03-08 16:42:57 -0500

    Oops, refactored everything instead

--- SmarterScheduling/SmarterScheduling/MapComponent_Brain.cs
+++ SmarterScheduling/SmarterScheduling/MapComponent_Brain.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using Verse;
+using Verse.AI.Group;
 
 namespace SmarterScheduling
 {
@@ -12,45 +13,36 @@ namespace SmarterScheduling
         {
             SLEEP,
             JOY,
-            WORK
+            WORK,
+            ANYTHING
         }
 
-        public Dictionary<Pawn, PawnState> pawnStates;
+        public const float REST_THRESH_CRITICAL   = 0.05F ;
 
-        // Now defined per-pawn, please CTRL+F search for the same variable name.
-        // Can be found further down in this same file.
-        //public const float MOOD_THRESH_LOW  = 0.25F ;
-        //public const float MOOD_THRESH_HIGH = 0.52F ;
+        public const float HUNGER_THRESH_CRITICAL = 0.25F ;
 
-        public const float REST_THRESH_LOW   = 0.35F ;
-        public const float REST_THRESH_HIGH  = 0.90F ;
+        public const float REST_THRESH_LOW        = 0.35F ;
+        public const float REST_THRESH_HIGH       = 0.90F ;
 
-        public const float JOY_THRESH_LOW    = 0.28F ;
-        public const float JOY_THRESH_HIGH   = 0.90F ;
-
-        public const float HUNGER_THRESH_LOW = 0.25F ;
+        public const float JOY_THRESH_LOW         = 0.28F ;
+        public const float JOY_THRESH_HIGH        = 0.90F ;
 
         public const string PSYCHE_NAME = "Psyche";
 
-        public Area psyche;
+        public Dictionary<Pawn, PawnState> pawnStates;
         public Dictionary<Pawn, Area> lastPawnAreas;
+        public Dictionary<Pawn, int> doctorFaults;
+        public Faction playerFaction;
+        public Area psyche;
 
         public int slowDown = 0;
 
-        public bool anyoneAwaitingTreatment = false;
-        public Dictionary<Pawn, int> doctorFaults;
-        public bool alreadyResetDoctorThisTick = false;
-
         public MapComponent_Brain(Map map) : base(map)
         {
-            pawnStates = new Dictionary<Pawn, PawnState>();
-
-            lastPawnAreas = new Dictionary<Pawn, Area>();
-
-            anyoneAwaitingTreatment = false;
-            doctorFaults = new Dictionary<Pawn, int>();
-            alreadyResetDoctorThisTick = false;
-
+            this.pawnStates = new Dictionary<Pawn, PawnState>();
+            this.lastPawnAreas = new Dictionary<Pawn, Area>();
+            this.doctorFaults = new Dictionary<Pawn, int>();
+            this.playerFaction = getPlayerFaction();
             initPsycheArea();
             initPawnsIntoCollection();
         }
@@ -99,30 +91,44 @@ namespace SmarterScheduling
                 {
                     lastPawnAreas[p] = curPawnArea;
                 }
-                if (isPawnDoctor(p))
+                if (!doctorFaults.ContainsKey(p))
                 {
-                    if (!doctorFaults.ContainsKey(p))
-                    {
-                        doctorFaults.Add(p, 0);
-                    }
+                    doctorFaults.Add(p, 0);
                 }
             }
         }
 
-        public bool isAnyoneAwaitingTreatment()
+        public Faction getPlayerFaction()
         {
             Faction playerFaction = Find.FactionManager.FirstFactionOfDef(FactionDefOf.PlayerColony);
             if (playerFaction == null)
             {
                 playerFaction = Find.FactionManager.FirstFactionOfDef(FactionDefOf.PlayerTribe);
             }
+            return playerFaction;
+        }
+
+        public bool isAnyoneNeedingTreatment()
+        {
+            foreach (Pawn p in map.mapPawns.FreeColonistsAndPrisonersSpawned)
+            {
+                if (p.health.HasHediffsNeedingTendByColony())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool isAnyoneAwaitingTreatment()
+        {
             foreach (Pawn p in map.mapPawns.FreeColonistsAndPrisonersSpawned)
             {
                 if (
                     p.health.HasHediffsNeedingTendByColony()
                     && p.CurJob.def.reportString == "lying down."
                     && !p.pather.Moving
-                    && !map.reservationManager.IsReserved(p, playerFaction)
+                    && !map.reservationManager.IsReserved(p, this.playerFaction)
                     )
                 {
                     return true;
@@ -131,7 +137,8 @@ namespace SmarterScheduling
             return false;
         }
 
-        // If "treatment" is anywhere in your top 15 WorkGivers, you're a doctor.
+        // If "treatment" is anywhere in your
+        // top 15 WorkGivers, you're a doctor.
         // Otherwise, you're not a doctor.
         public bool isPawnDoctor(Pawn p)
         {
@@ -168,9 +175,24 @@ namespace SmarterScheduling
             }
         }
 
+        public bool isThereAParty()
+        {
+            foreach (Lord l in map.lordManager.lords)
+            {
+                if (l.faction == this.playerFaction)
+                {
+                    if (l.LordJob != null && l.LordJob is LordJob_Joinable_Party)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
         public bool tryToResetPawn(Pawn p)
         {
-            if (p.health.capacities.CanBeAwake
+            if (   p.health.capacities.CanBeAwake
                 && p.health.capacities.GetEfficiency(PawnCapacityDefOf.Moving) > 0
                 && !p.health.InPainShock
                 && !p.Drafted
@@ -193,50 +215,6 @@ namespace SmarterScheduling
             pawnStates[p] = state;
             TimeAssignmentDef newTad;
 
-            // BEGIN DETOUR - we try to let the algorithm treat Doctors fairly,
-            // but if somebody isn't getting medical treatment, then
-            // we intervene and start handling Doctors differently
-            // so that nobody dies.
-            if (this.anyoneAwaitingTreatment)
-            {
-                if (!alreadyResetDoctorThisTick)
-                {
-                    if (isPawnDoctor(p))
-                    {
-                        if (state == PawnState.WORK)
-                        {
-                            if (!isPawnCurrentlyTreating(p))
-                            {
-                                bool success = tryToResetPawn(p);
-                                if (success)
-                                {
-                                    alreadyResetDoctorThisTick = true;
-                                }
-                            }
-                        }
-                        if (state == PawnState.JOY)
-                        {
-                            doctorFaults[p] += 1;
-                            if (doctorFaults[p] > 8)
-                            {
-                                setPawnState(p, PawnState.WORK);
-                                bool success = tryToResetPawn(p);
-                                if (success)
-                                {
-                                    alreadyResetDoctorThisTick = true;
-                                }
-                                return;
-                            }
-                        }
-                    }
-                }
-            }
-            else
-            {
-                doctorFaults[p] = 0;
-            }
-            // END DETOUR
-
             if (state == PawnState.SLEEP)
             {
                 newTad = TimeAssignmentDefOf.Sleep;
@@ -245,9 +223,13 @@ namespace SmarterScheduling
             {
                 newTad = TimeAssignmentDefOf.Joy;
             }
-            else
+            else if (state == PawnState.WORK)
             {
                 newTad = TimeAssignmentDefOf.Work;
+            }
+            else
+            {
+                newTad = TimeAssignmentDefOf.Anything;
             }
 
             for (int i = 0; i < 24; i++)
@@ -255,23 +237,17 @@ namespace SmarterScheduling
                 p.timetable.SetAssignment(i, newTad);
             }
 
-            if (state == PawnState.JOY && p.needs.food.CurLevel > HUNGER_THRESH_LOW)
+        }
+
+        public void restrictPawn(Pawn p, bool restrict)
+        {
+            if (restrict)
             {
                 p.playerSettings.AreaRestriction = psyche;
             }
             else
             {
                 p.playerSettings.AreaRestriction = lastPawnAreas[p];
-            }
-
-            if (
-                state == PawnState.JOY
-                && p.CurJob.def.reportString == "lying down."
-                && !(p.needs.rest.GUIChangeArrow > 0)
-                && !p.health.HasHediffsNeedingTendByColony()
-                )
-            {
-                tryToResetPawn(p);
             }
         }
 
@@ -290,68 +266,207 @@ namespace SmarterScheduling
             initPsycheArea();
             initPawnsIntoCollection();
 
-            this.anyoneAwaitingTreatment = isAnyoneAwaitingTreatment();
-            this.alreadyResetDoctorThisTick = false;
+            bool anyoneNeedingTreatment = isAnyoneNeedingTreatment();
+            bool anyoneAwaitingTreatment = false;
+            Random randGen = null;
+            
+            if (anyoneNeedingTreatment)
+            {
+                anyoneAwaitingTreatment = isAnyoneAwaitingTreatment();
+                if (anyoneAwaitingTreatment)
+                {
+                    randGen = new Random();
+                }
+            }
+
+            bool party = isThereAParty();
+            //bool alreadyResetDoctorThisTick = false;
 
             foreach (Pawn p in map.mapPawns.FreeColonistsSpawned)
             {
-                float MOOD_THRESH_LOW = p.mindState.mentalBreaker.BreakThresholdMajor + 0.02F;
-                float MOOD_THRESH_HIGH = p.mindState.mentalBreaker.BreakThresholdMinor + 0.08F;
+                float MOOD_THRESH_CRITICAL = p.mindState.mentalBreaker.BreakThresholdExtreme + 0.02F;
+                float MOOD_THRESH_LOW      = p.mindState.mentalBreaker.BreakThresholdMajor   + 0.02F;
+                float MOOD_THRESH_HIGH     = p.mindState.mentalBreaker.BreakThresholdMinor   + 0.08F;
 
-                bool areDoctor = isPawnDoctor(p);
-                if (anyoneAwaitingTreatment && areDoctor)
+                bool isDoctor = false;
+                if (anyoneNeedingTreatment)
                 {
-                    setPawnState(p, PawnState.WORK);
-                    continue;
+                    isDoctor = isPawnDoctor(p);
+                } else
+                {
+                    doctorFaults[p] = 0;
                 }
 
-                if (p.needs.rest.CurLevel < REST_THRESH_LOW)
+                if (p.needs.rest.CurLevel < REST_THRESH_CRITICAL || p.needs.food.CurLevel < HUNGER_THRESH_CRITICAL)
+                {
+                    setPawnState(p, PawnState.ANYTHING);
+                    restrictPawn(p, false);
+                }
+                else if (p.needs.mood.CurLevel < MOOD_THRESH_CRITICAL)
+                {
+                    if (p.needs.rest.CurLevel < REST_THRESH_LOW)
+                    {
+                        setPawnState(p, PawnState.SLEEP);
+                        restrictPawn(p, false);
+                    }
+                    else if (p.needs.rest.GUIChangeArrow > 0)
+                    {
+                        setPawnState(p, PawnState.SLEEP);
+                        restrictPawn(p, false);
+                    }
+                    else
+                    {
+                        setPawnState(p, PawnState.JOY);
+                        restrictPawn(p, false);
+                    }
+                }
+                else if (anyoneNeedingTreatment && isDoctor)
+                {
+                    if (anyoneAwaitingTreatment)
+                    {
+                        bool pawnTreating = isPawnCurrentlyTreating(p);
+                        if (pawnTreating)
+                        {
+                            doctorFaults[p] = 0;
+                            // Deliberately not setPawnState()
+                            // Deliberately not restrictPawn()
+                        }
+                        else
+                        {
+                            doctorFaults[p] += randGen.Next(5);
+                            if (doctorFaults[p] > 20)
+                            {
+                                setPawnState(p, PawnState.WORK);
+                                restrictPawn(p, false);
+                                tryToResetPawn(p);
+                            }
+                            else
+                            {
+                                setPawnState(p, PawnState.ANYTHING);
+                                restrictPawn(p, false);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        setPawnState(p, PawnState.ANYTHING);
+                        restrictPawn(p, false);
+                    }
+                }
+                else if (party)
+                {
+                    setPawnState(p, PawnState.ANYTHING);
+                    restrictPawn(p, false);
+                }
+                else if (p.needs.rest.CurLevel < REST_THRESH_LOW)
                 {
                     setPawnState(p, PawnState.SLEEP);
-                    continue;
+                    restrictPawn(p, false);
                 }
                 else if (p.needs.rest.GUIChangeArrow > 0)
                 {
                     setPawnState(p, PawnState.SLEEP);
-                    continue;
+                    restrictPawn(p, false);
+                }
+                else if (anyoneNeedingTreatment && p.health.HasHediffsNeedingTendByColony())
+                {
+                    setPawnState(p, PawnState.ANYTHING);
+                    restrictPawn(p, false);
                 }
                 else if (pawnStates[p] == PawnState.SLEEP && !(p.needs.rest.GUIChangeArrow > 0) && p.needs.rest.CurLevel > REST_THRESH_HIGH)
                 {
                     setPawnState(p, PawnState.JOY);
-                    continue;
+                    restrictPawn(p, true);
                 }
                 else if (pawnStates[p] == PawnState.JOY && p.needs.mood.CurLevel < MOOD_THRESH_HIGH)
                 {
                     setPawnState(p, PawnState.JOY);
-                    continue;
+                    restrictPawn(p, true);
                 }
                 else if (pawnStates[p] == PawnState.JOY && p.needs.joy.CurLevel < JOY_THRESH_HIGH)
                 {
                     setPawnState(p, PawnState.JOY);
-                    continue;
+                    restrictPawn(p, true);
                 }
                 else if (pawnStates[p] == PawnState.JOY && p.needs.mood.GUIChangeArrow > 0)
                 {
                     setPawnState(p, PawnState.JOY);
-                    continue;
+                    restrictPawn(p, true);
                 }
                 else if (p.needs.mood.CurLevel < MOOD_THRESH_LOW)
                 {
                     setPawnState(p, PawnState.JOY);
-                    continue;
+                    restrictPawn(p, true);
                 }
                 else if (p.needs.joy.CurLevel < JOY_THRESH_LOW)
                 {
                     setPawnState(p, PawnState.JOY);
-                    continue;
+                    restrictPawn(p, true);
                 }
                 else
                 {
                     setPawnState(p, PawnState.WORK);
-                    continue;
+                    restrictPawn(p, false);
                 }
+
+                if (
+                       pawnStates[p] == PawnState.JOY
+                    && p.CurJob.def.reportString == "lying down."
+                    && !(p.needs.rest.GUIChangeArrow > 0)
+                    && !p.health.HasHediffsNeedingTendByColony()
+                )
+                {
+                    tryToResetPawn(p);
+                }
+
             }
         }
 
     }
 }
+
+/*
+// BEGIN DETOUR - we try to let the algorithm treat Doctors fairly,
+            // but if somebody isn't getting medical treatment, then
+            // we intervene and start handling Doctors differently
+            // so that nobody dies.
+            if (this.anyoneAwaitingTreatment)
+            {
+    if (!alreadyResetDoctorThisTick)
+    {
+        if (isPawnDoctor(p))
+        {
+            if (state == PawnState.WORK)
+            {
+                if (!isPawnCurrentlyTreating(p))
+                {
+                    bool success = tryToResetPawn(p);
+                    if (success)
+                    {
+                        alreadyResetDoctorThisTick = true;
+                    }
+                }
+            }
+            if (state == PawnState.JOY)
+            {
+                doctorFaults[p] += 1;
+                if (doctorFaults[p] > 8)
+                {
+                    setPawnState(p, PawnState.WORK);
+                    bool success = tryToResetPawn(p);
+                    if (success)
+                    {
+                        alreadyResetDoctorThisTick = true;
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
+            else
+            {
+    doctorFaults[p] = 0;
+}
+            // END DETOUR
+*/
