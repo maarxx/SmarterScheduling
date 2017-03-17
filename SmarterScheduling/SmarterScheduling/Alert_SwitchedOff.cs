16a9c751adf6da574b84f8a7c4fc0e8ed9554bed
maarxx <chris.lewark@gmail.com>
2017-03-16 23:05:31 -0400

    Implement Issue #50

---
+++ SmarterScheduling/SmarterScheduling/Alert_SwitchedOff.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace SmarterScheduling
+{
+    class Alert_SwitchedOff : Alert_Critical
+    {
+        public Alert_SwitchedOff()
+        {
+            this.defaultLabel = "SmarterScheduling Disabled!";
+            this.defaultExplanation = "You have SmarterScheduling mod installed, but it is currently switched off. This was probably temporary. Don't forget to turn it back on!";
+        }
+        public override AlertReport GetReport()
+        {
+            if (!Find.VisibleMap.GetComponent<MapComponent_SmarterScheduling>().enabled)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
